fc06cc6a67a26eb92a25a2e87ebb6d602c5d11e4
Stephen Cleary <liveid.20.stephen-cleary@spamgourmet.com>
2014-05-07 23:17:36 -0400

    Migrated ThreadPoolEnlightenment. All necessary enlightenments are now migrated, though iOS/Droid support is temporarily disabled.

---
+++ Source/Enlightenment/Nito.AsyncEx.Enlightenment (NET4, Win8, SL5, WP8, WPA81)/Internal/PlatformEnlightenment/ThreadPoolEnlightenment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+
+namespace Nito.AsyncEx.Internal.PlatformEnlightenment
+{
+    public static class ThreadPoolEnlightenment
+    {
+        public static IDisposable RegisterWaitForSingleObject(WaitHandle handle, Action<object, bool> callback, object state, TimeSpan timeout)
+        {
+            throw Enlightenment.Exception();
+        }
+    }
+}
---
+++ Source/Enlightenment/Nito.AsyncEx.Enlightenment (NET45)/Internal/PlatformEnlightenment/ThreadPoolEnlightenment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+
+namespace Nito.AsyncEx.Internal.PlatformEnlightenment
+{
+    public static class ThreadPoolEnlightenment
+    {
+        public static IDisposable RegisterWaitForSingleObject(WaitHandle handle, Action<object, bool> callback, object state, TimeSpan timeout)
+        {
+            var registration = ThreadPool.RegisterWaitForSingleObject(handle, (innerState, timedOut) => callback(innerState, timedOut), state, timeout, true);
+            return new WaitHandleRegistration(registration);
+        }
+
+        private sealed class WaitHandleRegistration : IDisposable
+        {
+            private readonly RegisteredWaitHandle _registration;
+
+            public WaitHandleRegistration(RegisteredWaitHandle registration)
+            {
+                _registration = registration;
+            }
+
+            public void Dispose()
+            {
+                _registration.Unregister(null);
+            }
+        }
+    }
+}
