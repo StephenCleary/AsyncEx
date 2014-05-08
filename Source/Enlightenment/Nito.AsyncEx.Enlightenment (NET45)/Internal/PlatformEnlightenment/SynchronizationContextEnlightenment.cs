20bb1e4be3b0314a6fc697c7b1632d2d19de9dd7
Stephen Cleary <liveid.20.stephen-cleary@spamgourmet.com>
2014-05-07 22:52:43 -0400

    Migrated SynchronizationContextEnlightenment

---
+++ Source/Enlightenment/Nito.AsyncEx.Enlightenment (NET45)/Internal/PlatformEnlightenment/SynchronizationContextEnlightenment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Nito.AsyncEx.Internal.PlatformEnlightenment
+{
+    public static class SynchronizationContextEnlightenment
+    {
+        public static void SetCurrentSynchronizationContext(SynchronizationContext context)
+        {
+            SynchronizationContext.SetSynchronizationContext(context);
+        }
+    }
+}
