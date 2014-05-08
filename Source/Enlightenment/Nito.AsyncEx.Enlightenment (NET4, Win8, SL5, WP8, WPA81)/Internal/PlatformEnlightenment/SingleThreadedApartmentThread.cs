d9d516f1555369d582a5225701f86281f925040c
Stephen Cleary <liveid.20.stephen-cleary@spamgourmet.com>
2014-05-07 22:48:55 -0400

    Migrated STA thread.

---
+++ Source/Enlightenment/Nito.AsyncEx.Enlightenment (NET4, Win8, SL5, WP8, WPA81)/Internal/PlatformEnlightenment/SingleThreadedApartmentThread.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Nito.AsyncEx.Internal.PlatformEnlightenment
+{
+    public sealed class SingleThreadedApartmentThread
+    {
+        public SingleThreadedApartmentThread(Action execute, bool sta)
+        {
+            throw Enlightenment.Exception();
+        }
+
+        public Task JoinAsync()
+        {
+            throw Enlightenment.Exception();
+        }
+    }
+}
---
+++ Source/Enlightenment/Nito.AsyncEx.Enlightenment (Win8)/Internal/PlatformEnlightenment/SingleThreadedApartmentThread.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Nito.AsyncEx.Internal.PlatformEnlightenment
+{
+    public sealed class SingleThreadedApartmentThread
+    {
+        private readonly Task _thread;
+
+        public SingleThreadedApartmentThread(Action execute, bool sta)
+        {
+            if (sta)
+                throw new NotSupportedException("STA threads are not supported on this platform.");
+            _thread = Task.Factory.StartNew(execute, CancellationToken.None, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+        }
+
+        public Task JoinAsync()
+        {
+            return _thread;
+        }
+    }
+}
