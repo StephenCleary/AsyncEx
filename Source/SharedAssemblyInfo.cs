f9ed0500cbb2df804517eab2e3e22eafe49a890a
Stephen Cleary <github.ourteddybear@xoxy.net>
2017-01-29 15:39:20 -0500

    v4.0.1

--- Source/SharedAssemblyInfo.cs
+++ Source/SharedAssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A helper library for using the Task-based Asynchronous Pattern (TAP).")]
 [assembly: AssemblyCompany("Stephen Cleary")]
 [assembly: AssemblyProduct("Nito.AsyncEx Library")]
-[assembly: AssemblyCopyright("Copyright © 2011-2014 Stephen Cleary")]
+[assembly: AssemblyCopyright("Copyright © 2011-2017 Stephen Cleary")]
 [assembly: CLSCompliant(false)]
 
 [assembly: AssemblyVersion("4.0.1")]
-[assembly: AssemblyInformationalVersion("4.0.1-pre")]
+[assembly: AssemblyInformationalVersion("4.0.1")]
