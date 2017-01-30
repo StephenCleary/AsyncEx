8049465457b6decdd599c04b8719fb56789bbe15
Stephen Cleary <github.ourteddybear@xoxy.net>
2017-01-29 19:37:34 -0500

    Add AsyncEx.Interop.Eap.

--- Coverage.ps1
+++ Coverage.ps1
@@ -1,3 +1,3 @@
-$testProjectLocations = @('test/AsyncEx.Tasks.UnitTests', 'test/AsyncEx.Coordination.UnitTests', 'test/AsyncEx.Context.UnitTests', 'test/AsyncEx.Oop.UnitTests')
+$testProjectLocations = @('test/AsyncEx.Tasks.UnitTests', 'test/AsyncEx.Coordination.UnitTests', 'test/AsyncEx.Context.UnitTests', 'test/AsyncEx.Oop.UnitTests', 'test/AsyncEx.Interop.Eap.UnitTests')
 $outputLocation = 'testResults'
 iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/StephenCleary/BuildTools/599beba35b53f495d4df6e5c323573aa839137a3/Coverage.ps1'))
---
+++ src/Nito.AsyncEx.Interop.Eap/EapInterop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Nito.AsyncEx
+{
+    /// <summary>
+    /// Provides helpers for interop with older EAP-based APIs.
+    /// </summary>
+    public static class EapInterop
+    {
+    }
+}
