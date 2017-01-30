2e793637af0b1c955188f4b4f0283c962200fb72
Stephen Cleary <github.ourteddybear@xoxy.net>
2017-01-29 21:28:50 -0500

    Add AsyncEx.Interop.WaitHandles.

---
+++ src/Nito.AsyncEx.Interop.WaitHandles/WaitHandleInterop.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Nito.AsyncEx
+{
+    /// <summary>
+    /// Provides interop utilities for <see cref="WaitHandle"/> types.
+    /// </summary>
+    public static class WaitHandleInterop
+    {
+        /// <summary>
+        /// Wraps a <see cref="WaitHandle"/> with a <see cref="Task"/>. When the <see cref="WaitHandle"/> is signalled, the returned <see cref="Task"/> is completed. If the handle is already signalled, this method acts synchronously.
+        /// </summary>
+        /// <param name="handle">The <see cref="WaitHandle"/> to observe.</param>
+        public static Task FromWaitHandle(WaitHandle handle)
+        {
+            return FromWaitHandle(handle, Timeout.InfiniteTimeSpan, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Wraps a <see cref="WaitHandle"/> with a <see cref="Task{Boolean}"/>. If the <see cref="WaitHandle"/> is signalled, the returned task is completed with a <c>true</c> result. If the observation times out, the returned task is completed with a <c>false</c> result. If the handle is already signalled or the timeout is zero, this method acts synchronously.
+        /// </summary>
+        /// <param name="handle">The <see cref="WaitHandle"/> to observe.</param>
+        /// <param name="timeout">The timeout after which the <see cref="WaitHandle"/> is no longer observed.</param>
+        public static Task<bool> FromWaitHandle(WaitHandle handle, TimeSpan timeout)
+        {
+            return FromWaitHandle(handle, timeout, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Wraps a <see cref="WaitHandle"/> with a <see cref="Task{Boolean}"/>. If the <see cref="WaitHandle"/> is signalled, the returned task is (successfully) completed. If the observation is cancelled, the returned task is cancelled. If the handle is already signalled or the cancellation token is already cancelled, this method acts synchronously.
+        /// </summary>
+        /// <param name="handle">The <see cref="WaitHandle"/> to observe.</param>
+        /// <param name="token">The cancellation token that cancels observing the <see cref="WaitHandle"/>.</param>
+        public static Task FromWaitHandle(WaitHandle handle, CancellationToken token)
+        {
+            return FromWaitHandle(handle, Timeout.InfiniteTimeSpan, token);
+        }
+
+        /// <summary>
+        /// Wraps a <see cref="WaitHandle"/> with a <see cref="Task{Boolean}"/>. If the <see cref="WaitHandle"/> is signalled, the returned task is completed with a <c>true</c> result. If the observation times out, the returned task is completed with a <c>false</c> result. If the observation is cancelled, the returned task is cancelled. If the handle is already signalled, the timeout is zero, or the cancellation token is already cancelled, then this method acts synchronously.
+        /// </summary>
+        /// <param name="handle">The <see cref="WaitHandle"/> to observe.</param>
+        /// <param name="timeout">The timeout after which the <see cref="WaitHandle"/> is no longer observed.</param>
+        /// <param name="token">The cancellation token that cancels observing the <see cref="WaitHandle"/>.</param>
+        public static Task<bool> FromWaitHandle(WaitHandle handle, TimeSpan timeout, CancellationToken token)
+        {
+            // Handle synchronous cases.
+            var alreadySignalled = handle.WaitOne(0);
+            if (alreadySignalled)
+                return TaskConstants.BooleanTrue;
+            if (timeout == TimeSpan.Zero)
+                return TaskConstants.BooleanFalse;
+            if (token.IsCancellationRequested)
+                return TaskConstants<bool>.Canceled;
+
+            // Register all asynchronous cases.
+            return DoFromWaitHandle(handle, timeout, token);
+        }
+
+        private static async Task<bool> DoFromWaitHandle(WaitHandle handle, TimeSpan timeout, CancellationToken token)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            using (new ThreadPoolRegistration(handle, timeout, tcs))
+            using (token.Register(state => ((TaskCompletionSource<bool>) state).TrySetCanceled(), tcs, useSynchronizationContext: false))
+                return await tcs.Task.ConfigureAwait(false);
+        }
+
+        private sealed class ThreadPoolRegistration : IDisposable
+        {
+            private readonly RegisteredWaitHandle _registeredWaitHandle;
+
+            public ThreadPoolRegistration(WaitHandle handle, TimeSpan timeout, TaskCompletionSource<bool> tcs)
+            {
+                _registeredWaitHandle = ThreadPool.RegisterWaitForSingleObject(handle,
+                    (state, timedOut) => ((TaskCompletionSource<bool>)state).TrySetResult(!timedOut), tcs,
+                    timeout, executeOnlyOnce: true);
+            }
+
+            void IDisposable.Dispose() => _registeredWaitHandle.Unregister(null);
+        }
+    }
+}
---
+++ test/AsyncEx.Interop.WaitHandles.UnitTests/WaitHandleInteropUnitTests.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nito.AsyncEx;
+using System.Linq;
+using System.Threading;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+using Nito.AsyncEx.Testing;
+
+namespace UnitTests
+{
+    public class WaitHandleInteropUnitTests
+    {
+        [Fact]
+        public void FromWaitHandle_SignaledHandle_SynchronouslyCompletes()
+        {
+            var mre = new ManualResetEvent(true);
+            var task = WaitHandleInterop.FromWaitHandle(mre);
+            Assert.True(task.IsCompleted);
+        }
+
+        [Fact]
+        public void FromWaitHandle_SignaledHandleWithZeroTimeout_SynchronouslyCompletesWithTrueResult()
+        {
+            var mre = new ManualResetEvent(true);
+            var task = WaitHandleInterop.FromWaitHandle(mre, TimeSpan.Zero);
+            Assert.True(task.IsCompleted);
+            Assert.True(task.Result);
+        }
+
+        [Fact]
+        public void FromWaitHandle_UnsignaledHandleWithZeroTimeout_SynchronouslyCompletesWithFalseResult()
+        {
+            var mre = new ManualResetEvent(false);
+            var task = WaitHandleInterop.FromWaitHandle(mre, TimeSpan.Zero);
+            Assert.True(task.IsCompleted);
+            Assert.False(task.Result);
+        }
+
+        [Fact]
+        public void FromWaitHandle_SignaledHandleWithCanceledToken_SynchronouslyCompletes()
+        {
+            var mre = new ManualResetEvent(true);
+            var task = WaitHandleInterop.FromWaitHandle(mre, new CancellationToken(true));
+            Assert.True(task.IsCompleted);
+        }
+
+        [Fact]
+        public void FromWaitHandle_UnsignaledHandleWithCanceledToken_SynchronouslyCancels()
+        {
+            var mre = new ManualResetEvent(false);
+            var task = WaitHandleInterop.FromWaitHandle(mre, new CancellationToken(true));
+            Assert.True(task.IsCompleted);
+            Assert.True(task.IsCanceled);
+        }
+
+        [Fact]
+        public void FromWaitHandle_SignaledHandleWithZeroTimeoutAndCanceledToken_SynchronouslyCompletesWithTrueResult()
+        {
+            var mre = new ManualResetEvent(true);
+            var task = WaitHandleInterop.FromWaitHandle(mre, TimeSpan.Zero, new CancellationToken(true));
+            Assert.True(task.IsCompleted);
+            Assert.True(task.Result);
+        }
+
+        [Fact]
+        public void FromWaitHandle_UnsignaledHandleWithZeroTimeoutAndCanceledToken_SynchronouslyCompletesWithFalseResult()
+        {
+            var mre = new ManualResetEvent(false);
+            var task = WaitHandleInterop.FromWaitHandle(mre, TimeSpan.Zero, new CancellationToken(true));
+            Assert.True(task.IsCompleted);
+            Assert.False(task.Result);
+        }
+
+        [Fact]
+        public async Task FromWaitHandle_HandleSignalled_Completes()
+        {
+            var mre = new ManualResetEvent(false);
+            var task = WaitHandleInterop.FromWaitHandle(mre);
+            Assert.False(task.IsCompleted);
+            mre.Set();
+            await task;
+        }
+
+        [Fact]
+        public async Task FromWaitHandle_HandleSignalledBeforeTimeout_CompletesWithTrueResult()
+        {
+            var mre = new ManualResetEvent(false);
+            var task = WaitHandleInterop.FromWaitHandle(mre, Timeout.InfiniteTimeSpan);
+            Assert.False(task.IsCompleted);
+            mre.Set();
+            var result = await task;
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task FromWaitHandle_TimeoutBeforeHandleSignalled_CompletesWithFalseResult()
+        {
+            var mre = new ManualResetEvent(false);
+            var task = WaitHandleInterop.FromWaitHandle(mre, TimeSpan.FromMilliseconds(10));
+            var result = await task;
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task FromWaitHandle_HandleSignalledBeforeCanceled_CompletesSuccessfully()
+        {
+            var mre = new ManualResetEvent(false);
+            var cts = new CancellationTokenSource();
+            var task = WaitHandleInterop.FromWaitHandle(mre, cts.Token);
+            Assert.False(task.IsCompleted);
+            mre.Set();
+            await task;
+        }
+
+        [Fact]
+        public async Task FromWaitHandle_CanceledBeforeHandleSignalled_CompletesCanceled()
+        {
+            var mre = new ManualResetEvent(false);
+            var cts = new CancellationTokenSource();
+            var task = WaitHandleInterop.FromWaitHandle(mre, cts.Token);
+            Assert.False(task.IsCompleted);
+            cts.Cancel();
+            await AsyncAssert.CancelsAsync(task);
+        }
+
+        [Fact]
+        public async Task FromWaitHandle_HandleSignalledBeforeTimeoutOrCanceled_CompletesWithTrueResult()
+        {
+            var mre = new ManualResetEvent(false);
+            var cts = new CancellationTokenSource();
+            var task = WaitHandleInterop.FromWaitHandle(mre, Timeout.InfiniteTimeSpan, cts.Token);
+            Assert.False(task.IsCompleted);
+            mre.Set();
+            var result = await task;
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task FromWaitHandle_TimeoutBeforeHandleSignalledOrCanceled_CompletesWithFalseResult()
+        {
+            var mre = new ManualResetEvent(false);
+            var cts = new CancellationTokenSource();
+            var task = WaitHandleInterop.FromWaitHandle(mre, TimeSpan.FromMilliseconds(10), cts.Token);
+            var result = await task;
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task FromWaitHandle_CanceledBeforeTimeoutOrHandleSignalled_CompletesCanceled()
+        {
+            var mre = new ManualResetEvent(false);
+            var cts = new CancellationTokenSource();
+            var task = WaitHandleInterop.FromWaitHandle(mre, Timeout.InfiniteTimeSpan, cts.Token);
+            Assert.False(task.IsCompleted);
+            cts.Cancel();
+            await AsyncAssert.CancelsAsync(task);
+        }
+    }
+}
