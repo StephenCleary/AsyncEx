e963f649b405e0e7655b50b53498411bc42ab7d4
John Du Hart <jduhart@hhogdev.com>
2017-03-27 15:46:33 -0400

    Updating build scripts for new tooling

--- Build.ps1
+++ Build.ps1
@@ -1,1 +1,5 @@
-iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/StephenCleary/BuildTools/452c01f4b81a6c914f2cb8adbc19558e7403a7ff/Build.ps1'))
+#iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/StephenCleary/BuildTools/452c01f4b81a6c914f2cb8adbc19558e7403a7ff/Build.ps1'))
+dotnet restore
+
+# Pack can now run at a Solution level :tada:
+dotnet pack -c Release
