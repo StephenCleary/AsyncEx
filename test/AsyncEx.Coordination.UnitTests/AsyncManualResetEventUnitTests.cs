8d0f10b12734d2bf0dea873fd70ed84687939931
bboyle1234 <bboyle1234@gmail.com>
2018-06-28 15:02:03 +1000

    Improved unit tests for reset events

--- test/AsyncEx.Coordination.UnitTests/AsyncManualResetEventUnitTests.cs
+++ test/AsyncEx.Coordination.UnitTests/AsyncManualResetEventUnitTests.cs
@@ -145,32 +145,41 @@ namespace UnitTests
         }
 
         [Fact]
-        public void Try_Wait() {
+        public void Try_Wait1() {
 
+            /// We'll be testing the TryWaitAsync extension methods on this manual reset event.
             var mre = new AsyncManualResetEvent(false);
-            var result = false;
+            
+            /// Setup a task to set the event in 100ms from now
             var t1 = Task.Run(async () => {
                 await Task.Delay(100);
                 mre.Set();
             });
-            var t2 = Task.Run(async () => {
-                using (var cts = new CancellationTokenSource(TimeSpan.FromMilliseconds(200)))
-                    result = await mre.TryWaitAsync(cts.Token);
-            });
-            Task.WhenAll(t1, t2).Wait();
-            Assert.True(result);
+            
+            using (var cts = new CancellationTokenSource(TimeSpan.FromMilliseconds(200))) {
+                /// result should be true because we waited up to 200ms for something that would happen in 100ms
+                var result = mre.TryWaitAsync(cts.Token).Result;
+                Assert.True(result);
+            }
+        }
 
-            mre.Reset();
+        [Fact]
+        public void Try_Wait2() {
+
+            /// We'll be testing the TryWaitAsync extension methods on this manual reset event.
+            var mre = new AsyncManualResetEvent(false);
+            
+            /// Setup a task to set the event in 100ms from now
             var t3 = Task.Run(async () => {
                 await Task.Delay(200);
                 mre.Set();
             });
-            var t4 = Task.Run(async () => {
-                using (var cts = new CancellationTokenSource(TimeSpan.FromMilliseconds(100)))
-                    result = await mre.TryWaitAsync(cts.Token);
-            });
-            Task.WhenAll(t3, t4).Wait();
-            Assert.False(result);
+
+            using (var cts = new CancellationTokenSource(TimeSpan.FromMilliseconds(100))) {
+                /// result should be false because we waited only 100ms for something that would happen in 200ms
+                var result = mre.TryWaitAsync(cts.Token).Result;
+                Assert.False(result);
+            }
         }
     }
 }
