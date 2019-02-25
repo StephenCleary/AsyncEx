553f930ddfe03955de88ab017a543e20bf93e17e
Stephen Cleary <github.ourteddybear@xoxy.net>
2019-02-25 09:42:57 -0500

    Updated test for BlockingCollection<T>, showing concurrent Takes take the "wrong" item.

--- test/AsyncEx.Coordination.UnitTests/AsyncCollectionUnitTests.cs
+++ test/AsyncEx.Coordination.UnitTests/AsyncCollectionUnitTests.cs
@@ -32,30 +32,31 @@ namespace AsyncEx.Coordination.UnitTests
             Assert.Equal("e", await e);
         }
 
-        [Fact]
-        public async Task BlockingCollectionCompletesInOrder()
-        {
-            var collection = new BlockingCollection<string>();
+[Fact]
+public async Task BlockingCollectionCompletesInOrder()
+{
+    var collection = new BlockingCollection<string>();
 
-            collection.Add("a");
-            var a = collection.Take();
-            var b = Task.Run(() => collection.Take());
-            while (b.Status != TaskStatus.Running) ;
-            var c = Task.Run(() => collection.Take());
-            while (c.Status != TaskStatus.Running) ;
-            collection.Add("b");
-            var d = Task.Run(() => collection.Take());
-            while (d.Status != TaskStatus.Running) ;
-            collection.Add("c");
-            collection.Add("d");
-            collection.Add("e");
-            var e = Task.Run(() => collection.Take());
-            while (e.Status != TaskStatus.Running) ;
-            Assert.Equal("a", a);
-            Assert.Equal("b", await b);
-            Assert.Equal("c", await c);
-            Assert.Equal("d", await d);
-            Assert.Equal("e", await e);
-        }
+    collection.Add("a");
+    var a = Task.Run(() => collection.Take());
+    while (a.Status != TaskStatus.Running) ;
+    var b = Task.Run(() => collection.Take());
+    while (b.Status != TaskStatus.Running) ;
+    var c = Task.Run(() => collection.Take());
+    while (c.Status != TaskStatus.Running) ;
+    var d = Task.Run(() => collection.Take());
+    while (d.Status != TaskStatus.Running) ;
+    var e = Task.Run(() => collection.Take());
+    while (e.Status != TaskStatus.Running) ;
+    collection.Add("b");
+    collection.Add("c");
+    collection.Add("d");
+    collection.Add("e");
+    Assert.Equal("a", await a);
+    Assert.Equal("b", await b);
+    Assert.Equal("c", await c);
+    Assert.Equal("d", await d);
+    Assert.Equal("e", await e);
+}
     }
 }
